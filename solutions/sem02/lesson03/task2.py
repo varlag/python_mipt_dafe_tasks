fca4b38adc238594a8cfffa610b0d48bc21df290
Varvara <varya_lagutina@mail.ru>
2026-03-06 17:22:32 +0300

    done hw1

--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -9,11 +9,32 @@ def convert_from_sphere(
     distances: np.ndarray,
     azimuth: np.ndarray,
     inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+    if not (distances.shape == azimuth.shape == inclination.shape): 
+        raise ShapeMismatchError
+
+    z = distances * np.cos(inclination)
+    x = distances * np.sin(inclination) * np.cos(azimuth)
+    y = distances * np.sin(inclination) * np.sin(azimuth)
+
+    return x, y, z
 
 
 def convert_to_sphere(
     abscissa: np.ndarray,
     ordinates: np.ndarray,
     applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]: 
+    
+    x, y, z = abscissa, ordinates, applicates
+
+    if not (x.shape == y.shape == z.shape): 
+        raise ShapeMismatchError
+
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    phi = np.arctan2(y, x)
+    r_xy = np.sqrt(x ** 2 + y ** 2)
+    theta = np.arctan2(r_xy, z)
+
+    return r, phi, theta
--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,16 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if ordinates.size < 3: 
+        raise ValueError
+
+    l = ordinates[:-2]
+    c = ordinates[1:-1]
+    r = ordinates[2:]
+
+    max_e = (l < c) & (c > r)
+    min_e = (l > c) & (c < r)
+    ans = np.arange(1, ordinates.size - 1)
+
+    return ans[min_e], ans[max_e]
