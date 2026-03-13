54833db091d9e5e263ede9ce9b975f13337c1e68
Varvara <varya_lagutina@mail.ru>
2026-03-13 20:02:41 +0300

    hw2

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,43 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    new_shape = list(image.shape)
+
+    new_shape[0] += 2 * pad_size
+    new_shape[1] += 2 * pad_size
+
+    ans = np.zeros(new_shape, dtype=image.dtype)
+
+    ans[pad_size : image.shape[0] + pad_size, pad_size : image.shape[1] + pad_size] = image
+
+    return ans
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+
+    N = image.shape[0]
+    M = image.shape[1]
+
+    pad_size = kernel_size // 2
+    if pad_size == 0:
+        return image
+
+    new_image = pad_image(image, pad_size)
+    sum_in_window = np.zeros(image.shape, dtype=np.float64)
+
+    for i in range(kernel_size):
+        for j in range(kernel_size):
+            sum_in_window += new_image[i : i + N, j : j + M]
+
+    return (sum_in_window / kernel_size**2).astype(np.uint8)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -2,9 +2,33 @@ import numpy as np
 
 
 def get_dominant_color_info(
-    image: np.ndarray[np.uint8],
+    image: np.ndarray,
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    image = image.flatten()
+
+    colors = np.arange(256, dtype=np.int32)
+    counts = (image[:, np.newaxis] == colors).sum(axis=0)
+
+    cnt_colors = np.zeros(257, dtype=np.int64)
+    cnt_colors[1:] = np.cumsum(counts)
+
+    max_sum, color_for_ans = -1, 0
+
+    for curr_color in range(256):
+        if counts[curr_color] == 0:
+            continue
+
+        left = max(0, curr_color - threshold + 1)
+        right = min(255, curr_color + threshold - 1)
+
+        curr_sum = int(cnt_colors[right + 1] - cnt_colors[left])
+
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+            color_for_ans = curr_color
+
+    return np.uint8(color_for_ans), max_sum / image.size
